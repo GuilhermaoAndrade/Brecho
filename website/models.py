63bd2791bf56080482a37a0fd295a6b873b18895
unknown <guilhermesouza102@fatec.sp.gov.br>
2019-11-21 22:07:24 -0300

    criação do banco de dados

--- website/models.py
+++ website/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Produtos(models.Model):
+    opcoes_tamanho = [ 
+        ('PP','Extra pequeno'),
+        ('P', 'Pequeno'),
+        ('M', 'Médio'),
+        ('G', 'Grande'),
+        ('GG', 'Muito grande'),
+        ('XG', 'Extra grande')
+
+    ]
+
+    nome = models.CharField(max_length=60)
+    preco = models.DecimalField(decimal_places=2,max_digits=1000, default=50)
+    disponivel = models.BooleanField(default=True)
+    quantidade = models.IntegerField(default=1)
+    tamanho = models.CharField(max_length=2,choices=opcoes_tamanho)
+
+class Pedido(models.Model):
+    metodo_pagamento = [
+       ('AV', 'Pagamento á vista'),
+       ('P2', 'Parcelado em 2x'),
+       ('P2', 'Parcelado em 3x'),
+
+
+    ]
+
+    nome = models.CharField(max_length=140)
+    email = models.EmailField()
+    cartao = models.IntegerField()
+    pagamento = models.CharField(max_length=2, choices=metodo_pagamento)
